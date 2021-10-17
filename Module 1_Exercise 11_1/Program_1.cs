53d3b5a132570c21d2c2ef02e4359a20c9a5f725
Nikita Divin <Nikita.Divin@bonava.com>
2021-10-17 17:51:12 +0300

    Добавьте файлы проекта.

---
+++ Module 1_Exercise 11_1/Program_1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_1_Exercise_11_1
+{
+    class Program_1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Создадим экземпляр класса для решения линейного уравнения 0 = kx + b.");
+            linearEquation myLinearEquation;
+
+            Console.WriteLine("Введите k");
+            myLinearEquation.k = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите b");
+            myLinearEquation.b = Convert.ToDouble(Console.ReadLine());
+
+            double solution = myLinearEquation.Root();
+            Console.WriteLine("Решение уравнения: x = {0}", solution);
+            Console.ReadKey();
+        }
+
+        struct linearEquation
+        {
+            public double k;
+            public double b;
+
+            public double Root()
+            {
+                double x = -b / k;
+                return x;
+            }
+        }
+    }
+}
